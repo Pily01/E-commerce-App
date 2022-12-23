e686cb3a3411792c12e6bb32ab002ae06b6f79ae
Dominguez <pilymdm@bu.edu>
2022-12-23 15:49:08 -0600

    refactor: use  CategoryItem instead of html in map

--- book-store/src/App.js
+++ book-store/src/App.js
@@ -1,45 +1,46 @@
-import './categories.styles.scss'
+import './categories.styles.scss';
+import CategoryItem from './components/category-item/category-item.component';
 
 const App = () => {
 
   const categories = [
     {
       id: 1,
-      title: 'Literature & Fiction'
+      title: 'Literature & Fiction',
+      imageUrl: 'https://m.media-amazon.com/images/I/91U7rItuAML.jpg'
     },
     {
       id: 2,
-      title: 'Biographies & Memoirs'
+      title: 'Biographies & Memoirs',
+      imageUrl: 'https://m.media-amazon.com/images/I/51bZujlJxlL._AC_SY780_.jpg'
     },
     {
       id: 3,
-      title: 'Mystery, Thriller & Suspense'
+      title: 'Mystery, Thriller & Suspense',
+      imageUrl: 'https://m.media-amazon.com/images/I/51bZujlJxlL._AC_SY780_.jpg'
     },
     {
       id: 4,
-      title: 'Science & Math'
+      title: 'Science & Math',
+      imageUrl: 'https://m.media-amazon.com/images/I/51bZujlJxlL._AC_SY780_.jpg'
     },
     {
       id: 5,
-      title: 'Science Fiction & Fantasy'
+      title: 'Science Fiction & Fantasy',
+      imageUrl: 'https://wallpapercave.com/wp/wp1863544.jpg'
     },
     {
       id: 6,
-      title: 'Politics & Social Sciences'
+      title: 'Politics & Social Sciences',
+      imageUrl: 'https://m.media-amazon.com/images/I/51bZujlJxlL._AC_SY780_.jpg'
     }
   ];
 
   return (
     <div className="categories-container">
       {categories.map(category => {
-        return (
-          <div className="category-container" key={category.id}>
-            <div className="background-image"/>
-            <div className="category-body-container">
-              <h2>{category.title}</h2>
-              <p>Shop Now</p>
-            </div>
-          </div>
+        return(
+          <CategoryItem key={category.id} category={category}/>
         )
       })}
 
