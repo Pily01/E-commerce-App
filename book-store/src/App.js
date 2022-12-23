0763771f7d154235a28bf64a81fefa4c9cade8d8
Dominguez <pilymdm@bu.edu>
2022-12-23 14:23:12 -0600

    feat: add basic layout for categories

--- book-store/src/App.js
+++ book-store/src/App.js
@@ -1,8 +1,47 @@
 
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Literature & Fiction'
+    },
+    {
+      id: 2,
+      title: 'Biographies & Memoirs'
+    },
+    {
+      id: 3,
+      title: 'Mystery, Thriller & Suspense'
+    },
+    {
+      id: 4,
+      title: 'Science & Math'
+    },
+    {
+      id: 5,
+      title: 'Science Fiction & Fantasy'
+    },
+    {
+      id: 6,
+      title: 'Politics & Social Sciences'
+    }
+  ];
+
   return (
-    <div className="App">
-      Hello World
+    <div className="categories-container">
+      {categories.map(category => {
+        return (
+          <div className="category-container" key={category.id}>
+            <div className="background-image"/>
+            <div className="category-container">
+              <h2>{category.title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        )
+      })}
+
     </div>
   );
 }
