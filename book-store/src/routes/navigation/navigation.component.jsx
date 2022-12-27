c408b255bd943d893c9e028ba8038468bf1ef036
Dominguez <pilymdm@bu.edu>
2022-12-26 18:34:42 -0600

    feat: add Links and logo

--- book-store/src/routes/navigation/navigation.component.jsx
+++ book-store/src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,22 @@
 import { Fragment } from "react"
-import { Outlet } from "react-router-dom"
+import { Outlet, Link } from "react-router-dom"
+
+import { ReactComponent as Logo} from "../../assets/crown.svg"
+
+import './navigation.styles.scss'
 
 const Navigation = () => {
     return(
         <Fragment>
             <div className="navigation">
-                <h1> I am the navigation bar</h1>
+                <Link className="logo-container" to='/'>
+                    <Logo className='logo'/>
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to='/shop'>
+                        Shop
+                    </Link>
+                </div>
             </div>
             <Outlet/>
         </Fragment>
