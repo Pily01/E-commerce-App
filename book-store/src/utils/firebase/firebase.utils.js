f70849f4c24c7949ee780e4e8e735df9a85e4571
Dominguez <pilymdm@bu.edu>
2022-12-26 19:28:18 -0600

    feat: set up firebase authentication

--- book-store/src/utils/firebase/firebase.utils.js
+++ book-store/src/utils/firebase/firebase.utils.js
@@ -1,1 +1,26 @@
-import { initializeApp } from 'firebase/app'
+
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDq1OWNLqDvgFQ33tc-64vIcHHz8Zd0R4U",
+  authDomain: "book-magic-store-db.firebaseapp.com",
+  projectId: "book-magic-store-db",
+  storageBucket: "book-magic-store-db.appspot.com",
+  messagingSenderId: "566946461824",
+  appId: "1:566946461824:web:7fdaa830f0a050f0178e65"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Create google authentication  provider object
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: 'select_account'
+})
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
