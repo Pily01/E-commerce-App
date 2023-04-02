dee7b75bb4091d41bfa0dd61b0988174d47706d3
Dominguez <pilymdm@bu.edu>
2023-04-02 13:59:52 -0600

    refactor: implement sign-in-form component

--- book-store/src/routes/sign-in/sign-in.component.jsx
+++ book-store/src/routes/sign-in/sign-in.component.jsx
@@ -1,39 +1,10 @@
-import { useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth';
-
-import { 
-    auth, 
-    signInWithGooglePopup, 
-    createUserDocumentFromAuth,
-    signInWithGoogleRedirect 
-} from '../../utils/firebase/firebase.utils'
-
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+import SignInForm from '../../components/sign-in-form/sign-in-form.component';
 
 const SignIn = () => {
-    useEffect(() => {
-        async function _getRedirectResult() {
-            const response = await getRedirectResult(auth);
-            if(response) {
-                const userDocRef = await createUserDocumentFromAuth(response.user);
-            }
-        }
-        _getRedirectResult();
-    }, []);
-
-    const logGoogleUser = async () => {
-        const {user} = await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user);
-    }
-
     return(
-        <div>
-            <button onClick={logGoogleUser}>
-                Sign In with Google Popup
-            </button>
-            <button onClick={signInWithGoogleRedirect}>
-                Sign In with Google Redirect
-            </button>
+        <div className='sign-in-container'>
+            <SignInForm/>
             <SignUpForm/>
         </div>
     )
