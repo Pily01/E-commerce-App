cfad6cacff58f94f5a847a995005d98244c671b0
Dominguez <pilymdm@bu.edu>
2023-04-02 14:04:21 -0600

    refactor: implement sing-in-form component and rename from sign in to authentication component

--- book-store/src/routes/authentication/authentication.component.jsx
+++ book-store/src/routes/authentication/authentication.component.jsx
@@ -1,13 +1,14 @@
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 import SignInForm from '../../components/sign-in-form/sign-in-form.component';
+import './authentication.styles.scss'
 
-const SignIn = () => {
+const Authentication = () => {
     return(
-        <div className='sign-in-container'>
+        <div className='authentication-container'>
             <SignInForm/>
             <SignUpForm/>
         </div>
     )
 }
 
-export default SignIn
+export default Authentication
