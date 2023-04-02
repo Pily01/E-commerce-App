cead1cf025f277f98559552cbb5a2650f82a2cd7
Dominguez <pilymdm@bu.edu>
2023-04-02 14:25:08 -0600

    fix: import scss file to apply styles

--- book-store/src/components/sign-in-form/sign-in-form.component.jsx
+++ book-store/src/components/sign-in-form/sign-in-form.component.jsx
@@ -9,6 +9,7 @@ import {
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
+import './sign-in-form.styles.scss'
 
 const defaultFormFields = {
     email: '',
@@ -34,7 +35,7 @@ const SignInForm = () => {
         _getRedirectResult();
     }, []);
 
-    const logGoogleUser = async () => {
+    const signInWithGoogle = async () => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
     }
@@ -43,30 +44,36 @@ const SignInForm = () => {
     <div className='sign-in-container'>
             <h2>I already have an account</h2>
             <span>Sign in with your email and password</span>
-            <FormInput
-                  label='Email'
-                  type='email'
-                  required
-                  onChange={handleChange}
-                  name='email'
-                  value={email}
-            />
+            <form>
+                <FormInput
+                    label='Email'
+                    type='email'
+                    required
+                    onChange={handleChange}
+                    name='email'
+                    value={email}
+                />
 
-            <FormInput
-                  label='Password'
-                  type='password'
-                  required
-                  onChange={handleChange}
-                  name='password'
-                  value={password}
-            />
-            <Button type='submit'>Submit</Button>
-            <Button buttonType='google' onClick={logGoogleUser}>
-                Sign In with Google Popup
-            </Button>
-            <Button buttonType='google' onClick={signInWithGoogleRedirect}>
-                Sign In with Google Redirect
-            </Button>
+                <FormInput
+                    label='Password'
+                    type='password'
+                    required
+                    onChange={handleChange}
+                    name='password'
+                    value={password}
+                />
+                <div className='buttons-group'>
+                    <Button type='submit'>Submit</Button>
+                    <Button type='button' buttonType='google' onClick={signInWithGoogle}>
+                        Sign In with Google Popup
+                    </Button>
+                    {/* <Button buttonType='google' onClick={signInWithGoogleRedirect}>
+                    Sign In with Google
+                    </Button> */}
+                </div>
+            </form>
+ 
+            
         </div>
     )
 }
