d24c37a618755c0de1d5cf93a648803f2aca114b
Dominguez <pilymdm@bu.edu>
2023-06-08 14:19:02 -0600

    feat: add user context

--- book-store/src/context/user.context.jsx
+++ book-store/src/context/user.context.jsx
@@ -1,2 +1,15 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
+export const UserContext = createContext({
+
+});
+
+// This provider allows all of the children components to accesss
+// the values inside the useState
+export const UserProvider = ({children}) => {
+    const [currentUser, setCurrentUser] = useState(null);
+    const value = { currentUser, setCurrentUser};
+
+
+    return <UserContext.Provider value={value}> {children} </UserContext.Provider>
+}
