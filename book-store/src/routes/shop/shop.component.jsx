ca093b9f50dd510a184f16d9314fa4b5566296b8
Dominguez <pilymdm@bu.edu>
2023-06-09 19:34:58 -0600

    refactor: imlement product card component

--- book-store/src/routes/shop/shop.component.jsx
+++ book-store/src/routes/shop/shop.component.jsx
@@ -2,16 +2,14 @@
 import { useContext } from 'react'
 
 import { ProductsContext } from '../../context/products.context'
-
+import ProductCard from '../../components/product-card/product-card.component'
 
 const Shop = () => {
     const {products} = useContext(ProductsContext)
     return(
         <div>
-            {products.map((item) => (
-                <div key={item.id}>
-                    <h1>{item.name}</h1>
-                </div>
+            {products.map((product) => (
+                <ProductCard key={product.id} product={product}/>
             ))}
         </div>
     )
