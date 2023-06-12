f66b9aff5afa90b56896d34837c42fa56d589fc2
Dominguez <pilymdm@bu.edu>
2023-06-12 14:26:23 -0600

    feat: add toggle functionality to cart

--- book-store/src/components/cart-icon/cart-icon.component.jsx
+++ book-store/src/components/cart-icon/cart-icon.component.jsx
@@ -1,11 +1,17 @@
 import { ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
+import { useContext } from 'react'
+
+import { CartContext } from '../../context/cart.context'
 
 import './cart-icon.styles.scss'
 
 const CartIcon = () => {
+    const {isCartOpen, setIsCartOpen} = useContext(CartContext);
+
+    const toggleIsCartOppen = () => setIsCartOpen(!isCartOpen);
 
     return(
-        <div className='cart-icon-container'>
+        <div className='cart-icon-container' onClick={toggleIsCartOppen}>
             <ShoppingIcon className='shopping-icon'/>
             <span className='item-count'>0</span>
         </div>
