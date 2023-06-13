e4a5377efa30557248355206294ece2bf624ff47
Dominguez <pilymdm@bu.edu>
2023-06-13 01:06:56 -0600

    feat: create first version f checkout item

--- book-store/src/components/checkout-item/checkout-item.component.jsx
+++ book-store/src/components/checkout-item/checkout-item.component.jsx
@@ -7,15 +7,22 @@ import './checkout-item.styles.scss'
 
 const CheckoutItem = ({ cartItem }) => {
     const { name, quantity, imageUrl, price } = cartItem;
-    const { addItemToCart } = useContext(CartContext);
+    const { addItemToCart, removeItemFromCart } = useContext(CartContext);
     return(
-        <div>
-            <img src={imageUrl} alt={`${name}`}/>
-            <span> {name} </span>
-            <span> {quantity} </span>
-            <span> {price} </span>
-            <span> decrement </span>
-            <span onClick={() => addItemToCart(cartItem)}> increment </span>
+        <div className='checkout-item-container'>
+            <div className='image-container'>
+                <img src={imageUrl} alt={`${name}`}/>
+            </div>
+            <span className='name'> {name} </span>
+            <span className='arrow' onClick={() => removeItemFromCart(cartItem)}> - </span>
+            <span className='quantity'> {quantity} </span>
+            <span className='arrow' onClick={() => addItemToCart(cartItem)}> + </span>
+            <span className='price'> {price} </span>
+            <div className='remove-button'>&#10005;</div>
+
+            
+            
+           
         </div>
     )
 }
