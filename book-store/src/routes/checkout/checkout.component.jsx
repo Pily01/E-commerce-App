1e098d126021a54b8847ff7d07de5d96ccb638fb
Dominguez <pilymdm@bu.edu>
2023-06-13 01:05:33 -0600

    refactor: implement checkout item component

--- book-store/src/routes/checkout/checkout.component.jsx
+++ book-store/src/routes/checkout/checkout.component.jsx
@@ -1,30 +1,39 @@
 import { useContext } from 'react';
-
 import { CartContext } from '../../context/cart.context';
+
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
 import './checkout.styles.scss'
 
 const Checkout = () => {
     const { cartItems, addItemToCart, removeItemFromCart } = useContext(CartContext);
     return(
-        <div>
-            <h1> Checkout Page</h1>
-            <div>
+        <div className='checkout-container'>
+            <div className='checkout-header'>
+                <div className='header-block'>
+                    <span>Product</span>
+                </div>
+                <div className='header-block'>
+                    <span>Description</span>
+                </div>
+                <div className='header-block'>
+                    <span>Quantity</span>
+                </div>
+                <div className='header-block'>
+                    <span>Price</span>
+                </div>
+                <div className='header-block'>
+                    <span>Remove</span>
+                </div>
+            </div>
                 {
                     cartItems.map((cartItem) => {
-                    const {name, quantity} = cartItem;
-                    return <div>
-                        <h2>{name}</h2>
-                        <span>{quantity}</span>
-                        <br/>
-                        <span onClick={() => removeItemFromCart(cartItem)}>decrement</span>
-                        <br/>
-                        <span onClick={() => addItemToCart(cartItem)}>increment</span>
-                        
-                    </div>
+                    return(
+                        <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
+                    )
                     })
                 }
-            </div>
+                <span className='total'>Total: 0</span>
         </div>
     )
 }
