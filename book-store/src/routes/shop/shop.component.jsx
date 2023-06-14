da7e77c52c53be262a216cdcd13189c8e8bbb33d
Dominguez <pilymdm@bu.edu>
2023-06-14 14:55:42 -0600

    fix: import Routes and Route form react-router-dom instead of react

--- book-store/src/routes/shop/shop.component.jsx
+++ book-store/src/routes/shop/shop.component.jsx
@@ -1,25 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
 
-import { useContext, Fragment } from 'react'
-import { CategoriesContext } from '../../context/categories.context'
-
-import ProductCard from '../../components/product-card/product-card.component'
-import CategoryPreview from '../../components/category-preview/category-preview.component'
+import CategoriesPreview from '../categories-preview/categories-preview.component';
 
 import './shop.styles.scss'
 
 const Shop = () => {
-    const {categoriesMap} = useContext(CategoriesContext)
     return(
-        <Fragment>
-            {
-                Object.keys(categoriesMap).map(title => (
-                    <Fragment key={title}>
-                        <CategoryPreview title={title} products={categoriesMap[title]}/>
-                    </Fragment>
-                ))  
-            }
-        </Fragment>
-        
+        <Routes>
+            <Route path='/' element={<CategoriesPreview/>}/>
+        </Routes>
     )
 }
 
