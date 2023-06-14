54347b41297bfb4e7aa6f8127ad28a4333ac4abc
Dominguez <pilymdm@bu.edu>
2023-06-13 21:36:09 -0600

    fix: add await before calling getCategoriesAndDocuments

--- book-store/src/context/products.context.jsx
+++ book-store/src/context/products.context.jsx
@@ -1,6 +1,8 @@
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 import { useState } from "react";
-import SHOP_DATA from '../shop-data.js'
+// import SHOP_DATA from '../shop-data.js'
+
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
 
 export const ProductsContext = createContext({
     setProducts: () => null,
@@ -10,6 +12,19 @@ export const ProductsContext = createContext({
 export const ProductsProvider = ({children}) => {
     const [products, setProducts] = useState([]);
     const value = {products, setProducts};
+
+    useEffect(() => {
+        // We have to do this because getCategories and Documents is an async function
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments();
+            console.log(categoryMap);
+        };
+
+        getCategoriesMap();
+    }, [])
+    // useEffect(() => {
+    //     addCollectionAndDocuments('categories', SHOP_DATA);
+    // }, [])
 
     return <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>;
 }
